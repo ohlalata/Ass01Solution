f9e8f06a7c0ee0b7d0b6df946b38cf32bb6d29d2
Minh-Trung <nguyenminhtrungse@gmail.com>
2022-07-02 22:43:59 +0700

    Write code repository

--- DataAccess/Repository/MemberRepository.cs
+++ DataAccess/Repository/MemberRepository.cs
@@ -1,6 +1,16 @@
-﻿namespace DataAccess.Repository
+﻿using BusinessObject;
+using DataAccess.ADO;
+
+namespace DataAccess.Repository
 {
     public class MemberRepository : IMemberRepository
     {
+        public bool AddMember(MemberObject member) => MyStoreDbContext.GetDbContext.CreateMember(member);
+        public bool UpdateMember(MemberObject member) => MyStoreDbContext.GetDbContext.UpdateMember(member);
+        public bool DeleteMember(int memberId) => MyStoreDbContext.GetDbContext.RemoveMember(memberId);
+        public IEnumerable<MemberObject>? GetMembersList() => MyStoreDbContext.GetDbContext.GetListMembers();
+        public MemberObject? SearchMemberById(int id) => MyStoreDbContext.GetDbContext.FindAccountMemberById(id);
+        public MemberObject GetDefaultAdminAccount() => MyStoreDbContext.GetDbContext.GetDefaultAdminAccount();
+        public MemberObject? SearchMemberByIdOrEmail(int id, string email) => MyStoreDbContext.GetDbContext.FindAccountMemberByIdOrEmail(id, email);
     }
 }
