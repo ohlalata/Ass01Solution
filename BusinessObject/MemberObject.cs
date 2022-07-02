2ae31755345c8ee27fee7b9fb58fe486363a0fdc
Minh-Trung <nguyenminhtrungse@gmail.com>
2022-07-02 22:34:58 +0700

    Add properties MemberObject

--- BusinessObject/MemberObject.cs
+++ BusinessObject/MemberObject.cs
@@ -2,5 +2,11 @@
 {
     public class MemberObject
     {
+        public int MemberId { get; init; }
+        public string MemberName { get; init; } = String.Empty;
+        public string Email { get; init; } = String.Empty;
+        public string Password { get; init; } = String.Empty;
+        public string City { get; init; } = String.Empty;
+        public string Country { get; init; } = String.Empty;
     }
 }
